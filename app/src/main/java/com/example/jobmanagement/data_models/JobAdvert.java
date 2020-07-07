ff3229f45bcbabcd5d495db706c6f6ceb609b5f0
kvs <tlholokgoele@hotmail.com>
2020-07-07 15:09:30 +0200

    Version 1.1

--- app/src/main/java/com/example/jobmanagement/data_models/JobAdvert.java
+++ app/src/main/java/com/example/jobmanagement/data_models/JobAdvert.java
@@ -7,6 +7,86 @@ public class JobAdvert
     @PrimaryKey
     private String id;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getJobSalary() {
+        return jobSalary;
+    }
+
+    public void setJobSalary(String jobSalary) {
+        this.jobSalary = jobSalary;
+    }
+
+    public String getJobLocation() {
+        return jobLocation;
+    }
+
+    public void setJobLocation(String jobLocation) {
+        this.jobLocation = jobLocation;
+    }
+
+    public String getAppointmentType() {
+        return appointmentType;
+    }
+
+    public void setAppointmentType(String appointmentType) {
+        this.appointmentType = appointmentType;
+    }
+
+    public String getJobPosition() {
+        return jobPosition;
+    }
+
+    public void setJobPosition(String jobPosition) {
+        this.jobPosition = jobPosition;
+    }
+
+    public String getJobCompany() {
+        return jobCompany;
+    }
+
+    public void setJobCompany(String jobCompany) {
+        this.jobCompany = jobCompany;
+    }
+
+    public String getJobDescription() {
+        return jobDescription;
+    }
+
+    public void setJobDescription(String jobDescription) {
+        this.jobDescription = jobDescription;
+    }
+
+    public boolean isLicense() {
+        return isLicense;
+    }
+
+    public void setLicense(boolean license) {
+        isLicense = license;
+    }
+
+    public String getJobQualification() {
+        return jobQualification;
+    }
+
+    public void setJobQualification(String jobQualification) {
+        this.jobQualification = jobQualification;
+    }
+
     private String jobTitle;
 
     private String jobSalary;
