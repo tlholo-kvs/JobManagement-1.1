04397356bfeb231d5cbe222745a4a71d2d749cab
kvs <tlholokgoele@hotmail.com>
2020-07-07 15:10:37 +0200

    Version 1.1

--- app/src/main/java/com/example/jobmanagement/data_models/JobApplication.java
+++ app/src/main/java/com/example/jobmanagement/data_models/JobApplication.java
@@ -5,6 +5,22 @@ import androidx.room.PrimaryKey;
 
 public class JobApplication
 {
+    public long getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(long jobId) {
+        this.jobId = jobId;
+    }
+
+    public long getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(long profileId) {
+        this.profileId = profileId;
+    }
+
     @PrimaryKey
     private long jobId, profileId;
 
